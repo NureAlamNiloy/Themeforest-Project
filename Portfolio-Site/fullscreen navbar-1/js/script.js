293fb8cbd234e503bebbdea441f5a8d3adf97e20
Niloy74779 <mdniloy74779@gmail.com>
2021-03-24 18:48:00 +0600

    niloy edit this

--- Portfolio-Site/fullscreen navbar-1/js/script.js
+++
@@ -1,9 +0,0 @@
-
-
- const openNavBtn = document.querySelector(".open-nav");
- const closeNavBtn = document.querySelector(".close-nav");
- openNavBtn.addEventListener("click",toggleNav);
- closeNavBtn.addEventListener("click",toggleNav);
- function toggleNav(){
-    document.querySelector(".nav").classList.toggle("open");
- }
---
+++ Portfolio/Server/js/script.js
@@ -0,0 +1,96 @@
+$(function () {
+    "use strict";
+    
+
+    
+    
+
+    // mobile menu
+
+    const openNavBtn = document.querySelector(".open-nav");
+    const closeNavBtn = document.querySelector(".close-nav");
+    if (openNavBtn && closeNavBtn) {
+        openNavBtn.addEventListener("click", toggleNav);
+        closeNavBtn.addEventListener("click", toggleNav);
+
+        function toggleNav() {
+            document.querySelector(".nav").classList.toggle("open");
+        }
+    }
+
+
+
+
+
+    // Testimonial Slider
+
+    $('.testimonial-slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        dotsClass: 'slider_dots',
+        responsive: [
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 1,
+              }
+            },
+          ]
+    });
+
+
+
+    // Personal Info Counter js
+
+    $('.counter-number').counterUp({
+        time: 3000
+    });
+
+
+    $('.counter-items').tilt({
+        maxTilt: 20,
+        scale: 1.03,
+    });
+
+    // Pricing Part 
+
+    var toggleSwitch = document.querySelector(".toggle-switch");
+
+    if (toggleSwitch) {
+        toggleSwitch.addEventListener("change", () => {
+            if (toggleSwitch.checked) {
+                document.querySelector(".pricing-main").classList.add("active");
+            } else {
+                document.querySelector(".pricing-main").classList.remove("active");
+            }
+        });
+    }
+
+
+    // portfolio Paga Start
+
+    $('.portfolio-nav button').click(function () {
+        $('.portfolio-nav button').removeClass('active');
+        $(this).addClass('active');
+    });
+
+    // filterizr
+    if ($.fn.filterizr) {
+      var filterizd = $('.filter-container').filterizr({});   
+    }
+       
+    
+    //  venubox
+    $('.venobox').venobox();
+
+    // portfolio Paga End
+
+
+
+
+
+});
